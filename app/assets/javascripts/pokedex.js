407f74379723ee1295ea5d286c84291ff2f808be
Garrett Rodriguez <eloso79@gmail.com>
2015-01-04 15:43:37 -0800

    Add isotope filters

---
+++ app/assets/javascripts/pokedex.js
@@ -0,0 +1,28 @@
+/* Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+*/
+
+$( document ).ready(function() {
+var $container = $('#container');
+$container.isotope({
+	itemSelector: '.pokemon',
+	layoutMode: 'fitRows'
+});
+
+
+$('#menu a').on('click',function(){
+console.log($(this).attr('class'));
+var $type = $(this).attr('class');
+	
+	if($type == 'all'){
+	$('#container').isotope({
+	filter: '*'
+		})
+	}else{
+	$('#container').isotope({
+	filter: '.'+$type
+		})
+		}
+});
+});
